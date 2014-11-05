13d59a555260549ecb717f44ef52c05802c2579f
Ally Weir <ally.pcgf@gmail.com>
2014-11-05 17:08:38 +0000

    Audio nag created with example audio file

---
+++ app/scripts/audio_nag.coffee
@@ -0,0 +1,4 @@
+## Audio Nagware using Buzz HTML5 audio library.
+console.log 'Audio Nag initialised'
+
+test_audio = new buzz.sound('/media/1up.mp3').play()
---
+++ app/scripts/audio_nag.js
@@ -0,0 +1,8 @@
+(function() {
+  var test_audio;
+
+  console.log('Audio Nag initialised');
+
+  test_audio = new buzz.sound('/media/1up.mp3').play();
+
+}).call(this);
